a9c12177bf400727a2c6e37e048b03edcce46b34
Martin Weber <martinweber88@googlemail.com>
2013-05-28 23:20:55 +0200

    License added to the header

--- CSV_XML_Parser/WriteXMLFile.java
+++ CSV_XML_Parser/WriteXMLFile.java
@@ -1,4 +1,26 @@
 //package com.mkyong.core;
+
+
+/*
+* Copyright (c) 2013 by The AMOS project, Group 3,
+* http://osr.cs.fau.de/2013/04/17/the-2013-amos-projects-start-today/
+*
+* This file is part of the AMOS OSVAS application.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this program. If not, see
+* <http://www.gnu.org/licenses/>.
+*/
  
 import java.io.File;
 import java.io.*;
